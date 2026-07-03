bf0b20ba555822e9e5b347f5311734caf665c214
Sruthika Palanisamy <sruthi38655@gmail.com>
2026-07-03 13:26:54 +0530

    updated some modules

---
+++ If-else.py
@@ -0,0 +1,7 @@
+num = 5
+if num>10:
+    print("True")
+elif num==3:
+    print("hehe")
+else:
+    print("False")
--- string.py
+++ string.py
@@ -1,1 +1,10 @@
-name = input("Enter the string with which you have to perform the operatiions")
+name = input("Enter the string with which you have to perform the operatiion: ")
+print(name)
+print(name.encode())
+print(name[::-1])
+print(name)
+b = "Hello, World!"
+print(b[2:5])
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
