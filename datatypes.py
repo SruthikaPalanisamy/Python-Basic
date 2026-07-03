a145bbda395fe3f4aaacf50d7beef8229a193820
Sruthika Palanisamy <sruthi38655@gmail.com>
2026-07-03 11:33:47 +0530

    included the datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2.2
+c  = "String"
+logged = True
+print( "the type of a is " ,type(a) , a)
+print( "the type of b is " ,type(b) , b)
+print( "the type of c is " ,type(c) , c)
+print( "the type of logged is " ,type(logged) , logged)
